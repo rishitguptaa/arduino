612bece698254ac097ab1917e0d61884d270b212
rishitguptaa <64081032+rishitguptaa@users.noreply.github.com>
2020-06-17 17:06:28 +0530

    Add files via upload

---
+++ iNPUT_CAR.ino
@@ -0,0 +1,31 @@
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void forward();
+
+char x;
+
+void loop()
+{
+  if (Serial.available())
+  {
+    x = Serial.read();
+    Serial.write(x);
+
+    if (x == 'f')
+    {
+      forward();
+      digitalRead(2, HIGH);
+      digitalRead(3, LOW);
+      digitalRead(4, HIGH);
+      digitalRead(5, LOW);
+
+    }
+  }
+}
